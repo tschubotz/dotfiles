61267a010a4b68ddefab34e007909f2f6fd33e7b
Tobias Schubotz <tobias.schubotz@gmail.com>
2013-11-03 23:01:29 +0100

    added scripts to gather and install files

---
+++ gather.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+cp ~/Library/Preferences/com.googlecode.iterm2.plist ./
+cp ~/.zshrc ./
---
+++ install.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+cp com.googlecode.iterm2.plist ~/Library/Preferences/
+cp .zshrc ~/
